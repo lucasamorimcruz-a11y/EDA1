650bcb348f40d68ea4ac8e2f1d9f53984881126b
Lucas <lucas.amorim.cruz@gmail.com>
2026-09-01 02:20:43 -0300

    update

--- fila.c
+++ fila.c
@@ -11,7 +11,8 @@ typedef struct _fila
     struct _node *frente;
     int topo;
 } _fila;
-_fila *Fila(int elemento){
+_fila *Fila(int elemento)
+{
     _fila *fila_nova = malloc(sizeof(_fila));
     fila_nova->topo = elemento;
     fila_nova->frente = malloc(sizeof(_node));
@@ -22,22 +23,29 @@ _fila *Fila(int elemento){
 void inserir(_fila *fila, int elemento)
 {
     _node *aux = fila->frente;
-    while (aux->atras != NULL){
+    while (aux->atras != NULL)
+    {
         aux = aux->atras;
     }
     _node *novo_elemento = malloc(sizeof(_node));
-    aux->atras  = novo_elemento;
+    aux->atras = novo_elemento;
     novo_elemento->atras = NULL;
     novo_elemento->valor = elemento;
     aux = novo_elemento;
 }
 void remover(_fila *fila)
 {
+    if (fila->frente != NULL)
+    {
+        return;
+    }
     _node *aux = fila->frente;
     fila->frente = fila->frente->atras;
-    fila->topo = fila->frente->valor;
     free(aux);
-
+    if (fila->frente != NULL)
+    {
+        fila->topo = fila->frente->valor;
+    }
 }
 void topo(_fila *fila)
 {
@@ -47,7 +55,8 @@ void mostrar_elementos(_fila *fila)
 {
     _node *aux = fila->frente;
     int index = 1;
-    while (aux != NULL){
+    while (aux != NULL)
+    {
         printf("%i --- ELEMENTO :  %i\n", index, aux->valor);
         index++;
         aux = aux->atras;
@@ -57,10 +66,10 @@ int main()
 {
     _fila *fila = Fila(5);
     inserir(fila, 10);
-    inserir (fila, 20);
-    inserir (fila, 30);
-    inserir (fila, 40);
-    inserir (fila, 50);
+    inserir(fila, 20);
+    inserir(fila, 30);
+    inserir(fila, 40);
+    inserir(fila, 50);
     mostrar_elementos(fila);
     remover(fila);
     mostrar_elementos(fila);
